71d288e8dbde8db19ae414e30e03b7e73e66587a
Jacob Shkrob <Jacob Shkrob@10.152.85.75>
2016-05-12 08:42:34 -0500

    Created Tile class

---
+++ Minesweeper/src/minesweeper/Tile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author Jacob Shkrob
+ */
+public class Tile {
+    public int X_POSITION;
+    public int Y_POSITION;
+    public boolean hasBomb;
+    
+    public Tile(int x, int y, boolean hasB){
+        
+    
+    }
+}
